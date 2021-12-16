39618eeb224b07eabeb9d7369f6b5efc485876cf
Vikalp Malhotra <kimboho@gmail.com>
2021-12-15 21:20:28 -0500

    commit tree c1cef536a0e7e418d97cd2f63eee765d38055ee4

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+
+findMatching = (drivers, name) => {
+    let passDrivers = drivers.filter((idx) => idx === name);
+
+
+    return drivers.filter(
+        (idx) => idx.toLowerCase() === name.toLowerCase()
+    )
+    return passDrivers
+}
+
+
+function fuzzyMatch(drivers, string) {
+    const driverSpecific = drivers.filter(function (element) {
+        return element.slice(0, 2) === string;
+    });
+    return driverSpecific;
+
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function (hometown) {
+        return hometown.name === string;
+    });
+}
